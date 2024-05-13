159a4802af22931ef941b45fa6a958ec9064d171
Tarik Kavaz <tarikkavaz@gmail.com>
2024-05-13 13:45:25 +0300

    chore: Update footer layout and add logo and contact information

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="md:col-span-8">
         <p class="text-sm text-black">
-          <strong>santral</strong>istanbul Kampüsü<br>Eski Silahtarağa Elektrik Santralı Kazım Karabekir Cad. No: 2/13 34060 Eyüp İstanbul<br><a :href="'mailto:' + email"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="inline-block w-4 h-4 mr-1 svg-inline--fa fa-envelope"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" class=""></path></svg>{{ email }}</a>
+          <strong>santral</strong>istanbul Kampüsü<br>Eski Silahtarağa Elektrik Santralı Kazım Karabekir Cad. No: 2/13 34060 Eyüp İstanbul<br><a :href="'mailto:' + email"><EnvelopeIcon class="inline-block mr-1 size-4" aria-hidden="true" />{{ email }}</a>
         </p>
       </div>
     </div>
@@ -16,4 +16,5 @@
 <script setup lang="ts">
   const config = useRuntimeConfig();
   const email = config.public.email;
+  import { EnvelopeIcon } from '@heroicons/vue/24/outline'
 </script>
