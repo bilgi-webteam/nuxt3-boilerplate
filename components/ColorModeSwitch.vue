b9a6a239d973e63b83b9fc4ecfb77d257edb591e
Tarik Kavaz <tarikkavaz@gmail.com>
2024-05-31 12:20:22 +0300

    Refactor ColorModeSwitch component to update dark mode icon colors

--- components/ColorModeSwitch.vue
+++ components/ColorModeSwitch.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex items-center gap-4 mt-1">
     <!-- Icon Toggle -->
-    <Switch v-model="isDarkMode" :class="[isDarkMode ? 'bg-malachite-600' : 'bg-gray-400', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-malachite-600 focus:ring-offset-2']" @click="toggleColorMode">
+    <Switch v-model="isDarkMode" :class="[isDarkMode ? 'bg-slate-600' : 'bg-gray-400', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-malachite-600 focus:ring-offset-2']" @click="toggleColorMode">
       <span class="sr-only">Toggle Dark Mode</span>
       <span :class="[isDarkMode ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out']">
         <span :class="[isDarkMode ? 'opacity-0 duration-100 ease-out' : 'opacity-100 duration-200 ease-in', 'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity']" aria-hidden="true">
           <SunIcon class="w-3 h-3 text-gray-900" />
         </span>
         <span :class="[isDarkMode ? 'opacity-100 duration-200 ease-in' : 'opacity-0 duration-100 ease-out', 'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity']" aria-hidden="true">
-          <MoonIcon class="w-3 h-3 text-malachite-600" />
+          <MoonIcon class="w-3 h-3 text-slate-600" />
         </span>
       </span>
     </Switch>
-    <span class="text-sm font-semibold leading-6 text-slate-900 dark:text-slate-100">
+    <!-- <span class="text-sm font-semibold leading-6 text-slate-900 dark:text-slate-100">
       {{ isDarkMode ? $t('dark') : $t('light') }}
-    </span>
+    </span> -->
 
   </div>
 </template>
