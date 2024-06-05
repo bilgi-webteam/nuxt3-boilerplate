f0c177cf10ce292f74fe55ec27b8dd0815241506
Tarik Kavaz <tarikkavaz@gmail.com>
2024-06-05 11:53:13 +0300

    Refactor Footer and Vertical layout components to update styling and class names

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
-    class="min-h-18 container mx-auto max-w-screen-xl border-t-[1px] border-slate-500 p-4 text-slate-900 dark:text-slate-100"
+    class="container mx-auto min-h-20 max-w-screen-xl border-t-[1px] border-slate-500 p-4 text-slate-900 dark:text-slate-100"
   >
     <div class="mx-auto grid h-full gap-4 md:grid-cols-10 md:gap-8 md:px-8">
       <div class="md:col-span-2">
--- layouts/vertical.vue
+++ layouts/vertical.vue
@@ -7,7 +7,7 @@
       <main>
         <div class="mx-auto max-w-7xl grid-cols-5 gap-4 md:grid">
           <MenuDesktop
-            :navWrapperClass="'sticky items-center justify-between hidden border-r-[1px] border-r-slate-500 top-5 lg:justify-start md:block pr-2 bg-slate-100 dark:bg-slate-900'"
+            :navWrapperClass="'max-h-[calc(100vh-6rem)] sticky items-center justify-between hidden border-r-[1px] border-r-slate-500 top-5 lg:justify-start md:block pr-2 bg-slate-100 dark:bg-slate-900'"
             :navContainerClass="'md:pl-0 lg:flex lg:flex-col lg:gap-x-6 gap-y-2'"
             :dropdownTriggerWrapperClass="'relative group'"
             :dropdownTriggerClass="'px-3 py-1 text-sm font-semibold leading-6 border-b-2 border-b-transparent text-slate-900 dark:text-slate-100'"
@@ -15,7 +15,7 @@
             :dropdownContainerClass="'min-w-max'"
             :dropdownLinkClass="`block py-2 pb-0 mx-4 text-sm border-b-2 min-w-max border-b-transparent text-slate-900 last:mb-3 dark:text-white ml-3 before:mr-2 before:content-['-'] before:w-5`"
             :navItemClass="'px-3 py-1 text-sm font-semibold leading-6 border-b-2 border-b-transparent text-slate-900 dark:text-slate-100'"
-            :colorModeSwitchClass="'hidden mt-10 sm:block'"
+            :colorModeSwitchClass="'hidden ml-3 mt-5 sm:block'"
           />
           <div
             class="col-span-4 [&_a]:border-b-2 [&_a]:border-dotted [&_a]:border-malachite-500 hover:[&_a]:border-solid [&_a]:dark:border-malachite-200 [&_h2]:mb-6 [&_h2]:font-Libre [&_h2]:text-2xl [&_h2]:font-bold [&_h3]:mb-4 [&_h3]:font-Libre [&_h3]:text-xl [&_h3]:font-bold [&_h4]:mb-2 [&_h4]:font-Libre [&_h4]:text-lg [&_h4]:font-bold [&_h5]:mb-1 [&_h5]:font-Libre [&_h5]:text-base [&_h5]:font-bold [&_p]:mb-4 [&_p]:text-pretty"
